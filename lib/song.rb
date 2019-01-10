99aee26b5bcaaadef96f9655e7650273e0d2785a
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-10 03:01:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
 
   def self.new_by_filename(file)
     new_song = Song.new(file.split(" - ")[1])
-
+    new_song.artist = Artist.find_or_create_by_name(file.split(" - ")[0])
   end
 
   def self.all
