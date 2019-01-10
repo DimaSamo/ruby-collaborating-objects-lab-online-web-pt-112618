cf1f0d74ee2a183d2feea56788266e90f9bb1c68
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-10 02:48:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,12 +6,15 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@all.push(self)
   end
 
   def add_song(song)
     @songs.push(song)
     song.artist = self
+  end
+
+  def save
+    @@all.push(self)
   end
 
 end
