40acd043d2d43c67895d53fc586713a64a0e243b
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-10 03:08:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   def self.new_by_filename(file)
     new_song = Song.new(file.split(" - ")[1])
     #binding.pry
-  #  new_song.artist = Artist.find_or_create_by_name(file.split(" - ")[0])
+    new_song.artist = Artist.find_or_create_by_name(file.split(" - ")[0])
 #    new_song.artist.add_song(self)
     #binding.pry
   end
